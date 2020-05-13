26cbba24ca30b3d59e3c8c35784a40a592740f8d
TKCKWS <sara.aster+github@gmail.com>
2020-05-13 17:17:38 +0900

    メッセージプロパティファイルとバリデーションを紐づけるコンフィグ追加

--- SpringSample1/src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ SpringSample1/src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -17,29 +19,31 @@ import lombok.Data;
 public class SignupForm {
 
     // 必須入力、メールアドレス形式
-    @NotBlank
-    @Email
+    @NotBlank(message = "{require_check}")
+    @Email(message = "{email_check}")
     private String userId; // ユーザーID
 
     // 必須入力、
-    @NotBlank
+    @NotBlank(message = "{require_check}")
+    @Length(min = 4, max = 100, message = "{length_check}")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern_check}")
     private String password; // パスワード
 
     // 必須入力
-    @NotBlank
+    @NotBlank(message = "{require_check}")
     private String userName; // ユーザー名
 
     // 必須入力
-    @NotNull
+    @NotNull(message = "{require_check}")
     @DateTimeFormat(pattern = "yyyy/MM/dd") // DateTimeFormatアノテーションをフィールドにつけると文字列を日付型に変換
     private Date birthday; // 誕生日
 
     // 値が20から100まで
-    @Min(20)
-    @Max(100)
+    @Min(value=20, message="{min_check}")
+    @Max(value=100, message="{max_check}")
     private int age; // 年齢
 
     // falseのみ可能
-    @AssertFalse
+    @AssertFalse(message = "{marriage_check}")
     private boolean marriage; // 結婚ステータス
 }
