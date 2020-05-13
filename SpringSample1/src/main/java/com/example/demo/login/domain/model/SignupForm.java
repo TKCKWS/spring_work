8ebb14a933e1f3a67eee2c671af51a2ea0bc00bc
TKCKWS <sara.aster+github@gmail.com>
2020-05-13 16:13:46 +0900

    バリデーション追加

--- SpringSample1/src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ SpringSample1/src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -2,6 +2,13 @@ package com.example.demo.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -9,11 +16,30 @@ import lombok.Data;
 @Data
 public class SignupForm {
 
+    // 必須入力、メールアドレス形式
+    @NotBlank
+    @Email
     private String userId; // ユーザーID
+
+    // 必須入力、
+    @NotBlank
     private String password; // パスワード
+
+    // 必須入力
+    @NotBlank
     private String userName; // ユーザー名
+
+    // 必須入力
+    @NotNull
     @DateTimeFormat(pattern = "yyyy/MM/dd") // DateTimeFormatアノテーションをフィールドにつけると文字列を日付型に変換
     private Date birthday; // 誕生日
+
+    // 値が20から100まで
+    @Min(20)
+    @Max(100)
     private int age; // 年齢
+
+    // falseのみ可能
+    @AssertFalse
     private boolean marriage; // 結婚ステータス
 }
