0e927ae8d561e92713566c8b587073469bd74c59
TKCKWS <sara.aster+github@gmail.com>
2020-05-18 15:16:02 +0900

    Spring Security準備

--- SpringSample1/src/main/resources/data.sql
+++ SpringSample1/src/main/resources/data.sql
@@ -5,3 +5,7 @@ VALUES(1, '山田太郎', 30);
 /* ユーザーマスターテーブルのデータ（ADMIN権限） */
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
 VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+
+/* ユーザーマスターテーブルのデータ（ADMIN権限） */
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('ippan@xxx.co.jp', 'password', '一般次郎', '2000-01-01', 29, false, 'ROLE_GENERAL');
