a1838cac6de81fd26e87f39cd333e081675a2d70
TKCKWS <sara.aster+github@gmail.com>
2020-05-18 18:31:08 +0900

    REST(GET)

--- SpringSample1/src/main/java/com/example/demo/login/controller/UserRestController.java
+++ SpringSample1/src/main/java/com/example/demo/login/controller/UserRestController.java
@@ -1,8 +1,13 @@
 package com.example.demo.login.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.RestService;
 
 @RestController
@@ -10,4 +15,24 @@ public class UserRestController {
 
     @Autowired
     RestService service;
+
+    /**
+     * ユーザ全件取得用処理
+     */
+    @GetMapping("/rest/get")
+    public List<User> getUserMany() {
+        // ユーザ全件取得
+        return service.selectMany();
+    }
+
+    /**
+     * ユーザ1件取得用処理
+     */
+    @GetMapping("/rest/get/{id:.+}")
+    public User getUserOne(@PathVariable("id") String userId) {
+        // ユーザ1件取得
+        return service.selectOne(userId);
+    }
+
+
 }
